7292f1ae0f8fdc1781aa3fa5dd0b5c8421ea6dba
zengxiaofei <xiaofei_zeng@whu.edu.cn>
2025-10-22 17:18:12 +0800

    Fix some bugs and enhance usability for HapHiC refsort

--- scripts/HapHiC_refsort.py
+++ scripts/HapHiC_refsort.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def parse_agp(agp):
+def parse_agp(agp, min_ctg_len):
 
     logger.info('Parsing input AGP file...')
 
@@ -42,7 +42,7 @@ def parse_agp(agp):
             group, group_start, group_end = cols[0], int(cols[1]), int(cols[2])
             if group_end > group_len_dict[group]:
                 group_len_dict[group] = group_end
-            group_agp_lines[cols[0]].append(line)
+            group_agp_lines[group].append(line)
             if cols[4] != 'W':
                 continue
             ctg, ctg_start, ctg_end = cols[5], int(cols[6]), int(cols[7])
@@ -55,7 +55,7 @@ def parse_agp(agp):
 
     one_ctg_groups = set()
     for group, ctg_len_list in group_ctg_dict.items():
-        if len(ctg_len_list) == 1 and sum([l for _, l in ctg_len_list]) < 10000000:
+        if len(ctg_len_list) == 1 and sum([l for _, l in ctg_len_list]) < min_ctg_len * 1000000:
             one_ctg_groups.add(group)
             for i, ctg_info in enumerate(ctg_group_dict[ctg_len_list[0][0]][::-1]):
                 if ctg_info[0] == group:
@@ -73,12 +73,12 @@ def get_max_ovl_group(groups, ctg_aln_start, ctg_aln_end):
         ovl_len = 0 if ovl.empty else ovl.upper - ovl.lower + 1
         if ovl_len > max_ovl:
             max_ovl = ovl_len
-            best_group = group
+            max_ovl_group = group
 
     return max_ovl_group
 
 
-def parse_paf(paf, ctg_group_dict):
+def parse_paf(paf, ctg_group_dict, aln_len_cutoff):
 
     logger.info('Parsing input PAF file...')
 
@@ -95,11 +95,12 @@ def parse_paf(paf, ctg_group_dict):
                 orient = 1
             else:
                 orient = -1
-            if ctg_aln_end - ctg_aln_start < 5000:
+            if ctg_aln_end - ctg_aln_start < aln_len_cutoff:
                 continue
 
             if ctg in ctg_group_dict:
 
+                # solo contig
                 if len(ctg_group_dict[ctg]) == 0:
                     continue
 
@@ -131,6 +132,20 @@ def parse_paf(paf, ctg_group_dict):
                     group_ref_dict[group][ref][1] += ctg_aln_end - ctg_aln_start + 1
 
     return group_ref_dict
+
+
+def alignment_check(group_len_dict, group_ref_dict, one_ctg_groups, aln_len_cutoff):
+    
+    group_list = []
+
+    for group in group_len_dict:
+        if group not in group_ref_dict and group not in one_ctg_groups:
+            group_list.append(group)
+
+    if group_list:
+        err_message = 'Alignment check failed. Cannot find any alignment >= {} bp in the following group(s): {}'.format(aln_len_cutoff, ','.join(group_list))
+        logger.error(err_message)
+        raise Exception(err_message)
 
 
 def order_and_orient_groups(ctg_group_dict, group_ref_dict, group_agp_lines, group_len_dict, one_ctg_groups, args, fa_dict=None, fout=None):
@@ -252,11 +267,22 @@ def order_and_orient_groups(ctg_group_dict, group_ref_dict, group_agp_lines, gro
                 continue
             output_groups.add(group)
             if fout:
-                fout.write('>{}\n'.format(group))
+                if args.keep_original_ids:
+                    fout.write('>{}\n'.format(group))
+                else:
+                    if max_orient == 1:
+                        fout.write('>{}:{}:+\n'.format(group, ref))
+                    else:
+                        fout.write('>{}:{}:-\n'.format(group, ref))
+
             if max_orient == 1:
                 logger.info('{}: {} +'.format(ref, group))
                 for line in group_agp_lines[group]:
-                    print(line, end='')
+                    if args.keep_original_ids:
+                        print(line, end='')
+                    else:
+                        rest = line.split(maxsplit=1)[-1]
+                        print('{}:{}:+\t{}'.format(group, ref, rest), end='')
                     if fout:
                         cols = line.split()
                         if cols[4] == 'W':
@@ -282,8 +308,12 @@ def order_and_orient_groups(ctg_group_dict, group_ref_dict, group_agp_lines, gro
                         if fout:
                             Ns = int(cols[5])
                             seq_list.append('N'*Ns)
-                    print('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
-                        group, reversed_start, reversed_end, n, cols[4], cols[5], cols[6], cols[7], last_col))
+                    if args.keep_original_ids:
+                        print('{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+                            group, reversed_start, reversed_end, n, cols[4], cols[5], cols[6], cols[7], last_col))
+                    else:
+                        print('{}:{}:-\t{}\t{}\t{}\t{}\t{}\t{}\t{}\t{}'.format(
+                            group, ref, reversed_start, reversed_end, n, cols[4], cols[5], cols[6], cols[7], last_col))
             if fout:
                 seq = ''.join(seq_list)
                 for i in range(0, len(seq), args.max_width):
@@ -319,6 +349,22 @@ def parse_arguments():
     parser.add_argument(
             'paf', help='alignments between the draft assembly (query) and the reference genome (target) in PAF format generated by minimap2')
     parser.add_argument(
+            '--min_ctg_len', type=int, default=10,
+            help='minimum length for solo contigs to be ordered and oriented, default: %(default)s (Mbp). Scaffolds with multiple contigs are always processed')
+    parser.add_argument(
+            '--aln_len_cutoff', type=int, default=5000,
+            help='alignment length cutoff, default: %(default)s. Alignments shorter than this value are excluded from refsort')
+    parser.add_argument(
+            '--skip_aln_check', default=False, action='store_true',
+            help='skip scaffold alignment check, default: %(default)s. By default, the program will terminate if any scaffold, or any contig longer than '
+                 '`--min_ctg_len` lacks alignments meeting the length cutoff (`--aln_len_cutoff`), which may indicate inappropriate reference genome '
+                 'or alignment issues. Use this flag to bypass the check')
+    parser.add_argument(
+            '--keep_original_ids', default=False, action='store_true',
+            help='keep original sequence IDs in output files without appending reference information, default: %(default)s. By default, sequence IDs are '
+                 'formatted as "original_id:ref_chrom:orientation" (e.g., group1:chr1:+, group4:chr2:-) to indicate mapping relationships. When this flag is set, '
+                 'original IDs (e.g., group1, group4) are preserved instead')
+    parser.add_argument(
             '--fasta', default=None,
             help='raw (uncorrected) draft genome in FASTA format. When provided, the program will output sorted scaffolds in FASTA format')
     parser.add_argument(
@@ -326,7 +372,7 @@ def parse_arguments():
             help='maximum number of bases per line in the output FASTA file, default: %(default)s (bp)')
     parser.add_argument(
             '--fout', default='scaffolds.refsort.fa',
-            help='file name for output FASTA file, default: %(default)s')
+            help='file name for output FASTA file, default: %(default)s. This parameter takes effect only when `--fasta` is provided')
     parser.add_argument(
             '--ref_order', default=None,
             help='order of the reference chromosomes for outputting scaffolds, default: %(default)s (sorted by chromosome ID of the reference genome). '
@@ -349,8 +395,12 @@ def run(args, log_file=None):
     logger.info('Python version: {}'.format(sys.version.replace('\n', '')))
     logger.info('Command: {}'.format(' '.join(sys.argv)))
 
-    ctg_group_dict, group_agp_lines, group_len_dict, one_ctg_groups = parse_agp(args.agp)
-    group_ref_dict = parse_paf(args.paf, ctg_group_dict)
+    ctg_group_dict, group_agp_lines, group_len_dict, one_ctg_groups = parse_agp(args.agp, args.min_ctg_len)
+    
+    group_ref_dict = parse_paf(args.paf, ctg_group_dict, args.aln_len_cutoff)
+    if not args.skip_aln_check:
+        alignment_check(group_len_dict, group_ref_dict, one_ctg_groups, args.aln_len_cutoff)
+    
     if args.fasta:
         fa_dict = parse_fasta(args.fasta)
         with open(args.fout, 'w') as fout:
